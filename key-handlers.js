4989a829731766a6c25004da3589e6cc34842e99
Lautaro Bagnasco <lbagnasco@matchfin.ar>
2023-05-20 13:05:00 -0300

    add some touch (for phones) feedback

--- key-handlers.js
+++ key-handlers.js
@@ -3,6 +3,19 @@ const keys = {};
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
 
+document.addEventListener("touchstart", () => {
+  const fatty = Math.floor(50 * Math.random() + 10);
+    (fatty > 20 ? backElements : gameElements).push(
+      new Player(
+        Math.floor(Math.random() * gameWidth),
+        Math.floor(Math.random() * gameHeight),
+        fatty,
+        fatty,
+        fatty > 20 ? "#2c6f9b" : "#0099ff"
+      )
+    );
+})
+
 function handleKeyDown(event) {
   const key = event.key;
   keys[key] = true;
